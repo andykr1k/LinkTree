3ddc29a41a7786c24b139d6a521195f1954a258f
andykr1k <69021102+andykr1k@users.noreply.github.com>
2022-06-14 20:42:06 -0700

    Setting Up Linktree Structure

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Engineer","Developer", "Blogger", "Designer", "Freelancer","TheAppWizard"],
+        strings: ["Engineer","Developer", "Designer", "Economist"],
         typeSpeed: 100,
         backSpeed: 60,
         cursorChar: ' |',
